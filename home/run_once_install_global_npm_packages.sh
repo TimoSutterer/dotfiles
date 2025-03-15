6bd47c8cfe3f2da115db76079da160072eaa381f
Timo Sutterer <TimoSutterer@users.noreply.github.com>
2025-03-15 14:25:00 +0100

    feat: install czg

--- home/run_once_install_global_npm_packages.sh
+++ home/run_once_install_global_npm_packages.sh
@@ -7,7 +7,9 @@ if ! command -v npm > /dev/null 2>&1; then
 fi
 
 # Install global npm packages
+# Commit message linting
 npm install -g @commitlint/cli@19.6.1 @commitlint/config-conventional@19.6.0
+# Interactive Commitizen CLI 
+npm install -g czg@1.11.1
 
 echo "Global npm packages installed."
-
