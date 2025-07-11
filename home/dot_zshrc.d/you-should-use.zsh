b276d8e119219dae4e24cff319ccb7c83c17d6ab
Timo Sutterer <TimoSutterer@users.noreply.github.com>
2025-07-11 13:56:57 +0200

    feat: add zsh alias reminders

---
+++ home/dot_zshrc.d/you-should-use.zsh
@@ -0,0 +1,14 @@
+# You Should Use plugin for Zsh
+# This plugin reminds you to use aliases when you type full commands. It helps
+# build better shell habits by suggesting existing aliases for commands.
+#
+# Features:
+# - Displays reminders when you type commands that have shorter aliases
+# - Helps you learn and remember your configured aliases
+# - Improves command line efficiency over time
+# - Customizable reminder messages and behavior
+#
+# Example: If you type "git status" but have "gs" alias, it will remind you
+
+# Install the zsh-you-should-use plugin using zinit
+zinit light MichaelAquilina/zsh-you-should-use
