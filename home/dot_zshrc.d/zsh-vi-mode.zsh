272a75a4189d9367a9fc8581890482d29ab0a6e6
Timo Sutterer <TimoSutterer@users.noreply.github.com>
2025-09-07 13:26:49 +0200

    feat: add zsh vi mode

---
+++ home/dot_zshrc.d/zsh-vi-mode.zsh
@@ -0,0 +1,8 @@
+# Zsh Vi Mode Plugin
+# This plugin provides enhanced vi keybindings with visual mode indicators in
+# the prompt.
+
+# Install the zsh-vi-mode plugin using zinit with shallow clone for faster
+# installation
+zinit ice depth=1
+zinit light jeffreytse/zsh-vi-mode
